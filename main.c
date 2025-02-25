073b83193fff57ad9c068b7bb811ddfbb5d5b5cc
Ethan Rios <eirios2006@gmail.com>
2025-02-24 19:09:23 -0800

    Open text file through main.c and copy contents into an array. Print contents of the text file to user

--- main.c
+++ main.c
@@ -3,13 +3,16 @@
  * CWID: 880071006
  * Email: eirios@csu.fullerton.edu
  * Due: February 26 @ 11:59pm
- * CPSC Assignment 2 - String Search
+ * CPSC 223C Assignment 2 - String Search
 */
 
-// Purpose:
+// PURPOSE: 
+
+// "a2.sh" in the terminal will run the bash program!!
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Removes the built-in next line (\n) when using the fgets() function
 void trim(char *s) {
@@ -17,12 +20,48 @@ void trim(char *s) {
 }
 
 int main () {
-    printf("Welcome to String Search presented by Ethan Rios\n\n");
+    printf("\nWelcome to String Search presented by Ethan Rios!\n\n");
 
-    FILE *file_ptr;
-    char file_name[50];
-    char file_data;
-    printf("Please enter the name of a data file in the same directory where this program is running: ");
+    FILE* file_ptr;        // Internal name of the text file
+    char file_name[50];    // External name of the text file
+    char* file_data;       // Array of char from the text file
+
+    // Gives user multiple chances to enter a text file in case of a typing mistake
+    do 
+    {
+        printf("Please enter the name of a data file in the same directory where this program is running: ");
+        fgets(file_name, 49, stdin);
+        trim(file_name);
+
+    }
+    while ((file_ptr = fopen(file_name, "r")) == NULL);
+
+    if ((file_ptr = fopen(file_name, "r")) != NULL) 
+    {
+        // Set file_ptr to the end of the text file
+        // Use malloc to copy the size of the text file to file_data (create space)
+        fseek(file_ptr, 0, SEEK_END);
+        file_data = (char*) malloc ((ftell(file_ptr) + 1) * sizeof(char));
+
+        // Set file_ptr to the start of the text file
+        rewind(file_ptr);
+
+        // Loop through the text file, copying each character
+        // from file_ptr into the array file_data
+        char c;
+        int i;
+        for (i = 0; (c = fgetc(file_ptr)) != EOF; i++) 
+        {
+            file_data[i] = c;
+        }
+
+        // Print the number of bytes and contents of the file to the user
+        printf("Thank you! The size of the file you entered has %d bytes.", i);
+        printf("\nThe data from the file was copied into a string array in the program.");
+        printf("\nHere is the data that was stored in an array of chars:\n\n");
+        printf("%s", file_data);
+
+    }
 
 
 
